0bf72f16aaf174bcbe8998ba786049cfcc0135f1
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-07-06 19:02:06 +0500

    2 qs done

---
+++ app.js
@@ -0,0 +1,20 @@
+// TABLE
+// var user = prompt("Enter a number : ");
+// for(var i = 1; i <= 10; i++){
+//     console.log(user * i);
+// }
+
+// sum of array elements
+// var array = [];
+// var sum = 0;
+// for (var i = 1; i <= 5; i++) {
+//   var arrNumbers = +prompt("Enter a number to put in an array : ");
+//   array.push(arrNumbers);
+// }
+// console.log(array);
+
+// for (var j = 0; j < array.length; j++) {
+//     sum += array[j];
+//   }
+// console.log(sum);
+
