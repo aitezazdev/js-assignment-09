3f36b86f71b65399c31d540bc4490708576fcf1e
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-07-06 19:54:43 +0500

    assignment completed (some qs updated)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 //     console.log(user * i);
 // }
 
+
+
 // 2. sum of array elements
 // var array = [];
 // var sum = 0;
@@ -18,10 +20,41 @@
 //   }
 // console.log(sum);
 
-// 3. String Print in reverse
-var userString = prompt("Enter a string : ");
-var reverse = "";
-for(var i = userString.length - 1; i >= 0; i--){
-    reverse += userString[i];
-}
-console.log(reverse);
+
+
+// 3. finding largest number in array
+// var arr = [];
+// for (var i = 1; i <= 5; i++) {
+//   var arrNum = +prompt("Enter a number to put in an array : ");
+//   arr.push(arrNum);
+// }
+// console.log(arr);
+
+// var largest = 0;
+// for(var j = 0; j < arr.length; j++){
+//     if (arr[j] > largest){
+//         largest = arr[j];
+//     }
+// }
+// console.log(`The largest number is ${largest}`);
+
+
+
+// 4. String Print in reverse
+// var userString = prompt("Enter a string : ");
+// var reverse = "";
+// for(var i = userString.length - 1; i >= 0; i--){
+//     reverse += userString[i];
+// }
+// console.log(reverse);
+
+
+
+// 5. Finding factorial of a number
+// var factorial = +prompt("Enter a number to find factorial : ");
+// let facValue = 1;
+// for (var i = 1; i <= factorial; i++) {
+//   facValue *= i;
+// }
+
+// console.log(facValue);
